96e08c94dbdd8a1cd99fc5f191be9b2ca767b00d
Linger <lingangliang3@gmail.com>
2023-02-18 16:08:30 +0800

    improve comment with gpttools

--- dc_shell_cmd.R
+++ dc_shell_cmd.R
@@ -1,9 +1,28 @@
-# 读取 txt 文本文件，忽略 header，以 `#` 为分隔符号变成表格，写入 Excel 文件，写入时忽略标题行列
+### Read the table from `.txt` file with separator `#` and store it into Excel file
 
-# 读入文本文件
-mydata <- read.table("dc_shell_cmd.txt", header = FALSE, comment.char = "", sep = "#", strip.white = TRUE)
+# assign the name of the input file to the variable file_name.
+file_name <- "dc_shell_cmd.txt"
 
-# 将数据框写入 Excel 文件
+# assign the name of the output excel file to the variable out_excel_name.
+out_excel_name <- "output/myexcelfile.xlsx"
+
+# assign the name of the output csv file to the variable out_csv_name.
+out_csv_name <- "output/mydata.csv"
+
+# read the .txt file and store it in the variable input_table.
+input_table <- read.table(file_name, header = FALSE, comment.char = "", sep = "#", strip.white = TRUE)
+
+# assign the column names to the variable input_table.
+names(input_table) <- c("Name", "Description")
+
+# load the xlsx library.
 library(xlsx)
-write.xlsx2(mydata, "myexcelfile.xlsx", sheetName = "Sheet1", row.names = FALSE, col.names = FALSE)
+
+# write the data in the variable input_table to the excel file.
+write.xlsx2(input_table, out_excel_name, sheetName = "Sheet1", row.names = FALSE, col.names = FALSE)
+
+# write the data in the variable input_table to the csv file.
+write.table(input_table, file = out_csv_name, sep = ",", col.names = FALSE, row.names = FALSE)
+
+
 
